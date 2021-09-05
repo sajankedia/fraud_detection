a662d13f04e21b9953224c39ed13b06acb562ccd
sajankedia <sajankedia@gmail.com>
2021-09-05 12:04:21 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -2,3 +2,14 @@
 
 print("adding all ML realted code here")
 
+
+def model1():
+    print("adding code for ML Model1")
+
+
+def model2():
+    print("adding code for ML Model2")
+
+def model3():
+    print("adding code for ML Model3")
+
